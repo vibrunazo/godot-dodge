ede493fe5acb65911cfc0dcd55b5a82eb7be05f3
Vandré Brunazo <vibrunazo@gmail.com>
2022-01-17 10:56:06 -0300

    colision detection and start func

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400 # how fast the player will move in pixels/sec
 var screen_size # size of the game window
 
@@ -49,3 +51,14 @@ func animate(velocity):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
