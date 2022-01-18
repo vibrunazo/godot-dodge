2e4a446ad0b31ce1f94963a832430a4a88c22fdd
Vandré Brunazo <vibrunazo@gmail.com>
2022-01-18 14:00:05 -0300

    mob seeks

--- Main.gd
+++ Main.gd
@@ -2,6 +2,8 @@ extends Node
 
 export (PackedScene) var Mob
 var score = 0
+var mob_count = 0
+var cooldown = 2.0
 
 func _ready():
 	randomize()
@@ -21,6 +23,8 @@ func game_over():
 
 func new_game():
 	score = 0
+	mob_count = 0
+	cooldown = 2.0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
@@ -37,20 +41,32 @@ func _on_ScoreTimer_timeout():
 	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
-	$MobTimer.wait_time = clamp(2 - score * 0.02, 0.2, 2)
+	cooldown -= 0.04 if cooldown > 1.4 else 0.02 if cooldown > 0.9 else 0.005 
+	$MobTimer.wait_time = clamp(cooldown, 0.25, 2)
 	print('mob timer: %f' % $MobTimer.wait_time)
 	# Choose a random location on Path2D
 	$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene
 	var mob = Mob.instance() as Mob
 	add_child(mob)
-	# Set the mob's direction perpendicular to the path direction
-	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-	# Set the mob's position to a random location
-	mob.position = $MobPath/MobSpawnLocation.position
-	# Add some randomness to the direction
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-	# Set the Velocity (speed & direction)
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	mob_count += 1
+		
+	if (mob_count % 4 == 0):
+		print('mob seek')
+		var vel := Vector2()
+		vel = $Player.position - mob.position
+		vel = vel.normalized() * rand_range(mob.min_speed, mob.max_speed)
+		mob.linear_velocity = vel
+		mob.rotation = vel.angle()
+	else:
+		print('mob dumb')
+		# Set the mob's direction perpendicular to the path direction
+		var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+		# Set the mob's position to a random location
+		mob.position = $MobPath/MobSpawnLocation.position
+		# Add some randomness to the direction
+		direction += rand_range(-PI / 4, PI / 4)
+		mob.rotation = direction
+		# Set the Velocity (speed & direction)
+		mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+		mob.linear_velocity = mob.linear_velocity.rotated(direction)
