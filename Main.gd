644b3ac993505d7d18f821b11ad380d237b7ebee
Vandré Brunazo <vibrunazo@gmail.com>
2022-01-17 21:59:46 -0300

    finishing up

--- Main.gd
+++ Main.gd
@@ -16,6 +16,8 @@ func game_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -23,6 +25,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready!")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	print('start timer')
